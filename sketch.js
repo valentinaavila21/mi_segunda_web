29c9251090fff31d0759636d17667080a2992996
Valentina Ávila P. <valentina.avila.p@ug.uchile.cl>
2025-08-31 22:32:24 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,30 @@
 var valor = document.getElementById("pecinco").offsetWidth;
 
-            window.addEventListener("resize", function () {
-                valor = document.getElementById("pecinco").offsetWidth;
-            });
+window.addEventListener("resize", function () {
+valor = document.getElementById("pecinco").offsetWidth;
+});
 
-            function setup() {
-                createCanvas(valor, valor, WEBGL).parent("#pecinco");
-                select("#nombre").html("Valentina Avila");
-                select("#aprendizaje").html("modelado en 3D"); 
-                noStroke();
-            }
-            function draw() {
-                background(255, 216, 223);
-                orbitControl();
-                pointlight (250,250,250, valor/3, valor/2,50);
-                specularMaterial(255);
-                shininess(50);
-                metalness(1);
-                fill(255,100,150);
-                sphere(valor/4, valor/4);
-             }
+function setup() {
+    createCanvas(valor, valor, WEBGL).parent("#pecinco");
+    select("#nombre").html("Valentina Avila");
+    select("#aprendizaje").html("modelado en 3D"); 
+    noStroke();
+}
 
-            function windowResized() {
-                resizeCanvas(valor, valor);
+function draw() {
+    background(216, 191, 216);
+    orbitControl();
+    directionalLight(200, 200, 200, -1, -1, -1);
+    pointLight(255, 255, 255, 200, -150, 200);
+    specularMaterial(200, 200, 255);
+    shininess(50);
+    emissiveMaterial(50, 50, 100);
+    metalness(1);
+    fill(139, 7, 139);
+    sphere(40, 64, 64);
+}
+
+function windowResized() {
+    resizeCanvas(valor, valor);
+
             }
